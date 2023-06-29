d8186ae65a439ca18e182fc0cf633877ea1adef3
sigristarisa <arisasigrist.ch@gmail.com>
2023-06-29 12:51:58 +0200

    created raw drive type file

---
+++ prisma/seed/rawDriveType.tsx
@@ -0,0 +1,13 @@
+import { DriveTypeSeed } from "../../helpers/types";
+
+export const awd: DriveTypeSeed = {
+  name: "allrad",
+};
+
+export const fwd: DriveTypeSeed = {
+  name: "vorderradantrieb",
+};
+
+export const rwd: DriveTypeSeed = {
+  name: "hinterradantrieb",
+};
