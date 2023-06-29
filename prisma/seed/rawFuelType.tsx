8323ab3a6f9abb8b9eda16bd4de8a4d23af25d0a
sigristarisa <arisasigrist.ch@gmail.com>
2023-06-29 12:54:48 +0200

    created raw fuel type file

---
+++ prisma/seed/rawFuelType.tsx
@@ -0,0 +1,21 @@
+import { FuelTypeSeed } from "../../helpers/types";
+
+export const electric: FuelTypeSeed = {
+  name: "elektro",
+};
+
+export const gasoline: FuelTypeSeed = {
+  name: "benzin",
+};
+
+export const diesel: FuelTypeSeed = {
+  name: "diesel",
+};
+
+export const hybrid: FuelTypeSeed = {
+  name: "hybrid",
+};
+
+export const hydrogen: FuelTypeSeed = {
+  name: "hydrogen",
+};
