3ee025e2f0477adf286b9a52629581382aade8a8
sigristarisa <arisasigrist.ch@gmail.com>
2023-06-29 12:48:11 +0200

    created raw gearbox file

---
+++ prisma/seed/rawGearboxType.tsx
@@ -0,0 +1,9 @@
+import { GearboxTypeSeed } from "../../helpers/types";
+
+export const automatic: GearboxTypeSeed = {
+  name: "automat",
+};
+
+export const manual: GearboxTypeSeed = {
+  name: "manuell",
+};
