3b589d1a30767b8aa25037b6fd80eae2358a5728
sigristarisa <arisasigrist.ch@gmail.com>
2023-06-29 12:32:19 +0200

    created raw model file

---
+++ prisma/seed/rawModel.tsx
@@ -0,0 +1,49 @@
+import { ModelSeed } from "../../helpers/types";
+
+export const tiguan: ModelSeed = {
+  name: "tiguan",
+};
+
+export const golf: ModelSeed = {
+  name: "golf",
+};
+
+export const formentor: ModelSeed = {
+  name: "formentor",
+};
+
+export const octavia: ModelSeed = {
+  name: "octavia",
+};
+
+export const fabia: ModelSeed = {
+  name: "fabia",
+};
+
+export const puma: ModelSeed = {
+  name: "puma",
+};
+
+export const model3: ModelSeed = {
+  name: "model 3",
+};
+
+export const ibiza: ModelSeed = {
+  name: "ibiza",
+};
+
+export const polo: ModelSeed = {
+  name: "polo",
+};
+
+export const tucson: ModelSeed = {
+  name: "tucson",
+};
+
+export const tarraco: ModelSeed = {
+  name: "tarraco",
+};
+
+export const a3: ModelSeed = {
+  name: "a3",
+};
